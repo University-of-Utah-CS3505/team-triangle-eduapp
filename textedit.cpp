9a5c080f700107e12efa2a436b79d7f71caa47ce
skajw90 <skajw90@gmail.com>
2018-11-27 23:56:41 -0700

    pull changes

--- textedit.cpp
+++ textedit.cpp
@@ -1,5 +1,24 @@
 #include "textedit.h"
 
-textedit::textedit(int font_size, int w, int h) {}
+textedit::textedit(int font_size, int w, int h) :
+                    _font_size(font_size), _w(w), _h(h), _text(""){}
 
-void textedit::draw(sf::RenderTarget& target, sf::RenderStates states) const {}
+void textedit::insert_char(char c) { _text.push_back(c); }
+void textedit::set_text(const std::string& text) { _text = text; }
+std::string textedit::get_text() const { return _text; }
+void textedit::backspace() {
+    if(_text.size() == 0) return;
+    else _text.pop_back();
+}
+
+void textedit::move_cursor(int x, int y) {
+
+}
+
+void textedit::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+   sf::Text text;
+   //text.setFont(_font);
+   text.setString(_text);
+   text.setCharacterSize(_font_size);
+   target.draw(text, states);
+}
--- textedit.h
+++ textedit.h
@@ -1,23 +1,22 @@
 #ifndef TEXTEDIT_H
 #define TEXTEDIT_H
 
-#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics.hpp>
 #include <string>
 
 class textedit : public sf::Drawable {
 public:
     textedit(int font_size, int w, int h);
-
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
     void insert_char(char c);
     void backspace();
-
     void set_text(const std::string& text);
     std::string get_text() const;
-
     void move_cursor(int x, int y);
-
 private:
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    sf::Font _font;
+    int _font_size, _w, _h;
+    std::string _text;
 };
 
 #endif // TEXTEDIT_H
