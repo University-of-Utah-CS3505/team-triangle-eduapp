0825311492e0b89e57ef081536a609c21f313fdd
Chase VanBuskirk <chasevanb@gmail.com>
2018-11-27 21:13:45 -0700

    main menu (only last texture is drawn correctly)

--- main_menu.cpp
+++ main_menu.cpp
@@ -9,7 +9,7 @@
 
 const auto deg_radian_conv_factor = 0.017453292519943295769236907;
 
-const auto scale = 100.0;
+const auto scale = -100.0;
 
 main_menu::main_menu(sf::RenderWindow& window)
     : _window(window), _world(b2Vec2(0.f, -1.f)) {
@@ -29,7 +29,7 @@ main_menu::main_menu(sf::RenderWindow& window)
     play_button.body->CreateFixture(&play_button_fix_def);
 
     auto& level_button = _items.emplace_back();
-    level_button.texture.loadFromFile("../team-triangle-eduapp/assets/play_button.png");
+    level_button.texture.loadFromFile("../team-triangle-eduapp/assets/levels_button.png");
     level_button.sprite.setTexture(level_button.texture);
     auto level_button_def = b2BodyDef();
     auto level_button_shape = b2PolygonShape();
@@ -38,12 +38,12 @@ main_menu::main_menu(sf::RenderWindow& window)
     s = level_button.texture.getSize();
     level_button_shape.SetAsBox(s.x / scale, s.y / scale);
     level_button_fix_def.shape = &level_button_shape;
-    level_button_def.position.Set(0.f, 0.f);
+    level_button_def.position.Set(200.f, 200.f);
     level_button.body = _world.CreateBody(&level_button_def);
     level_button.body->CreateFixture(&level_button_fix_def);
 
     auto& quit_button = _items.emplace_back();
-    quit_button.texture.loadFromFile("../team-triangle-eduapp/assets/play_button.png");
+    quit_button.texture.loadFromFile("../team-triangle-eduapp/assets/quit_button.png");
     quit_button.sprite.setTexture(quit_button.texture);
     auto quit_button_def = b2BodyDef();
     auto quit_button_shape = b2PolygonShape();
@@ -56,10 +56,10 @@ main_menu::main_menu(sf::RenderWindow& window)
     quit_button.body = _world.CreateBody(&quit_button_def);
     quit_button.body->CreateFixture(&quit_button_fix_def);
 
-    auto width = _window.getSize();
-    _items[0].sprite.setPosition(width.x/2-305, 700);
-    _items[1].sprite.setPosition(width.x/2-305, 400);
-    _items[2].sprite.setPosition(width.x/2-305, 100);
+    auto size = _window.getSize();
+    _items[0].sprite.setPosition(size.x/2-200, (size.y / 2) - 290);
+    _items[1].sprite.setPosition(size.x/2-200, (size.y / 2) - 70);
+    _items[2].sprite.setPosition(size.x/2-200, (size.y / 2) + 150);
 }
 
 main_menu::~main_menu() {}
@@ -72,7 +72,7 @@ void main_menu::update(std::unique_ptr<game_state>&) {
 //        item.sprite.setPosition(pos.x * scale, pos.y * scale);
 //        item.sprite.setRotation(item.body->GetAngle() * deg_radian_conv_factor);
         _window.draw(item.sprite);
-        std::cout << item.sprite.getPosition().x << item.sprite.getPosition().y << std::endl;
+//        std::cout << item.sprite.getPosition().x << item.sprite.getPosition().y << std::endl;
     }
     _window.display();
 }
