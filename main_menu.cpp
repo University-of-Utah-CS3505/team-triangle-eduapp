aa0a09897fa862461030821e24f3431569751dc0
cody-austin <cody.austin@utah.edu>
2018-11-27 22:11:17 -0700

    fix main menu - dangling pointer on menu_item move, box2d issues

--- main_menu.cpp
+++ main_menu.cpp
@@ -1,65 +1,68 @@
 #include "main_menu.h"
 
+#include <QDebug>
 #include <SFML/Graphics.hpp>
 #include <game_state.h>
-#include <memory>
-#include <QDebug>
 #include <iostream>
-
+#include <memory>
 
 const auto deg_radian_conv_factor = 0.017453292519943295769236907;
 
-const auto scale = -100.0;
+const auto scale = 100.0;
+
+main_menu::menu_item::menu_item(b2World& world,
+                                const std::string& tex_path,
+                                std::function<bool()> f,
+                                float x,
+                                float y,
+                                float r)
+    : on_press(f), texture(std::make_unique<sf::Texture>()) {
+    texture->loadFromFile(tex_path);
+    sprite.setTexture(*texture);
+    auto body_def = b2BodyDef();
+    auto shape = b2PolygonShape();
+    auto fix_def = b2FixtureDef();
+    body_def.type = b2_dynamicBody;
+    auto s = texture->getSize();
+    shape.SetAsBox(s.x / scale, s.y / scale);
+    fix_def.shape = &shape;
+    body_def.position.Set(x, y);
+    body_def.angle = r;
+    body = world.CreateBody(&body_def);
+    body->CreateFixture(&fix_def);
+    body->ApplyLinearImpulse(b2Vec2(6, -6), body_def.position, true);
+}
 
 main_menu::main_menu(sf::RenderWindow& window)
-    : _window(window), _world(b2Vec2(0.f, -1.f)) {
+    : _window(window), _world(b2Vec2(0.f, 10.f)) {
 
-    auto& play_button = _items.emplace_back();
-    play_button.texture.loadFromFile("../team-triangle-eduapp/assets/play_button.png");
-    play_button.sprite.setTexture(play_button.texture);
-    auto play_button_def = b2BodyDef();
-    auto play_button_shape = b2PolygonShape();
-    auto play_button_fix_def = b2FixtureDef();
-    play_button_def.type = b2_dynamicBody;
-    auto s = play_button.texture.getSize();
-    play_button_shape.SetAsBox(s.x / scale, s.y / scale);
-    play_button_fix_def.shape = &play_button_shape;
-    play_button_def.position.Set(0.f, 0.f);
-    play_button.body = _world.CreateBody(&play_button_def);
-    play_button.body->CreateFixture(&play_button_fix_def);
+    _items.emplace_back(_world,
+                        "../team-triangle-eduapp/assets/play_button.png",
+                        []() { return false; },
+                        -4,
+                        4,
+                        1);
+    _items.emplace_back(_world,
+                        "../team-triangle-eduapp/assets/levels_button.png",
+                        []() { return false; },
+                        -4.5,
+                        6,
+                        1);
 
-    auto& level_button = _items.emplace_back();
-    level_button.texture.loadFromFile("../team-triangle-eduapp/assets/levels_button.png");
-    level_button.sprite.setTexture(level_button.texture);
-    auto level_button_def = b2BodyDef();
-    auto level_button_shape = b2PolygonShape();
-    auto level_button_fix_def = b2FixtureDef();
-    level_button_def.type = b2_dynamicBody;
-    s = level_button.texture.getSize();
-    level_button_shape.SetAsBox(s.x / scale, s.y / scale);
-    level_button_fix_def.shape = &level_button_shape;
-    level_button_def.position.Set(200.f, 200.f);
-    level_button.body = _world.CreateBody(&level_button_def);
-    level_button.body->CreateFixture(&level_button_fix_def);
+    _items.emplace_back(_world,
+                        "../team-triangle-eduapp/assets/quit_button.png",
+                        []() { return false; },
+                        -4,
+                        8,
+                        2);
 
-    auto& quit_button = _items.emplace_back();
-    quit_button.texture.loadFromFile("../team-triangle-eduapp/assets/quit_button.png");
-    quit_button.sprite.setTexture(quit_button.texture);
-    auto quit_button_def = b2BodyDef();
-    auto quit_button_shape = b2PolygonShape();
-    auto quit_button_fix_def = b2FixtureDef();
-    quit_button_def.type = b2_dynamicBody;
-    s = quit_button.texture.getSize();
-    quit_button_shape.SetAsBox(s.x / scale, s.y / scale);
-    quit_button_fix_def.shape = &quit_button_shape;
-    quit_button_def.position.Set(400.f, 400.f);
-    quit_button.body = _world.CreateBody(&quit_button_def);
-    quit_button.body->CreateFixture(&quit_button_fix_def);
-
-    auto size = _window.getSize();
-    _items[0].sprite.setPosition(size.x/2-200, (size.y / 2) - 290);
-    _items[1].sprite.setPosition(size.x/2-200, (size.y / 2) - 70);
-    _items[2].sprite.setPosition(size.x/2-200, (size.y / 2) + 150);
+    auto floor_def = b2BodyDef();
+    auto floor_shape = b2PolygonShape();
+    auto floor_fix_def = b2FixtureDef();
+    floor_shape.SetAsBox(_window.getSize().x / scale, 1);
+    floor_fix_def.shape = &floor_shape;
+    floor_def.position.Set(0, _window.getSize().y / scale - 0.5);
+    _world.CreateBody(&floor_def)->CreateFixture(&floor_fix_def);
 }
 
 main_menu::~main_menu() {}
@@ -68,11 +71,12 @@ void main_menu::update(std::unique_ptr<game_state>&) {
     _window.clear(sf::Color::Cyan);
     _world.Step(1 / 60.0f, 1, 1);
     for (auto& item : _items) {
-//        auto pos = item.body->GetPosition();
-//        item.sprite.setPosition(pos.x * scale, pos.y * scale);
-//        item.sprite.setRotation(item.body->GetAngle() * deg_radian_conv_factor);
+        auto pos = item.body->GetPosition();
+        item.sprite.setPosition(pos.x * scale, pos.y * scale);
+        item.sprite.setRotation(item.body->GetAngle() * deg_radian_conv_factor);
         _window.draw(item.sprite);
-//        std::cout << item.sprite.getPosition().x << item.sprite.getPosition().y << std::endl;
+        std::cout << item.sprite.getPosition().x << item.sprite.getPosition().y
+                  << std::endl;
     }
     _window.display();
 }
--- main_menu.h
+++ main_menu.h
@@ -2,12 +2,12 @@
 #define MAIN_MENU_H
 
 #include "game_state.h"
+#include <Box2D/Box2D.h>
+#include <Box2D/Dynamics/b2Fixture.h>
 #include <Box2D/Dynamics/b2Fixture.h>
 #include <SFML/Graphics.hpp>
 #include <functional>
 #include <memory>
-#include <Box2D/Dynamics/b2Fixture.h>
-#include <Box2D/Box2D.h>
 
 class main_menu : public game_state {
 public:
@@ -18,9 +18,16 @@ public:
 
 private:
     struct menu_item {
-        sf::Texture texture;
+        menu_item(b2World& world,
+                  const std::string& tex_path,
+                  std::function<bool()> on_press,
+                  float x,
+                  float y,
+                  float r);
+
+        std::unique_ptr<sf::Texture> texture;
         sf::Sprite sprite;
-        b2Body *body;
+        b2Body* body;
         std::function<bool()> on_press;
     };
 
