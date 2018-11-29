92c4b4cd6f9d684c2b23218ba47429218967538c
skajw90 <skajw90@gmail.com>
2018-11-29 14:27:05 -0700

    still working on editor, just display words and background done

--- textedit.h
+++ textedit.h
@@ -7,13 +7,14 @@
 class textedit : public sf::Drawable {
 public:
     textedit(int font_size, int w, int h);
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
     void insert_char(char c);
     void backspace();
     void set_text(const std::string& text);
     std::string get_text() const;
     void move_cursor(int x, int y);
+    void clear();
 private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     sf::Font _font;
     int _font_size, _w, _h;
     std::string _text;
