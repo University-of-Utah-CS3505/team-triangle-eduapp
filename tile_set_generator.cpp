1239006ad37a10c7cdbb005f71b48997df5e87f1
Chase VanBuskirk <chasevanb@gmail.com>
2018-11-29 20:27:22 -0700

    fix JSON parsing for tiles and tiledefs

--- tile_set_generator.cpp
+++ tile_set_generator.cpp
@@ -1,36 +1,64 @@
 #include "tile_set_generator.h"
-//File loads in levels.JSON, then creates levels when requested.
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
 #include <boost/foreach.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 #include <iostream>
+
 namespace pt = boost::property_tree;
-tile_set_generator::tile_set_generator()
-{
 
-//levels = boost::extents[]
-auto root = pt::ptree();
-pt::read_json("../team-triangle-eduapp/levels/levels.json", root);
-for(pt::ptree::value_type &v : root.get_child("levels")){
-        std::string s = v.second.data();
-        levels.emplace_back(s);
-
+tile_set_generator::tile_set_generator() {
+    // _levels = boost::extents[]
+    auto root = pt::ptree();
+    pt::read_json("../team-triangle-eduapp/levels/levels.json", root);
+    for (pt::ptree::value_type& level : root.get_child("levels")) {
+        auto level_path = level.second.data();
+        _levels.emplace_back(level_path);
     }
 }
 
+// **Seems like texture is not the right return here, maybe pass in a
+// tank and tiles map and set, void return**
 
-sf::Texture tile_set_generator::tile_set_generate_level(int level)
-{
-    std::string path = levels[level];
-    std::cout << path << std::endl;
+sf::Texture tile_set_generator::tile_set_generate_level(int level) {
+    auto texture = sf::Texture {};
     auto root = pt::ptree();
-    pt::read_json("../team-triangle-eduapp/levels"+path, root);
-    long bounds = (long) root.get_child("tiles").size();
-    std::cout << bounds << std::endl;
-    boost::multi_array<int,2> boost_array_level(boost::extents[bounds][bounds]);
-    for(auto& item : root.get_child("tiles")){
-        std::cout <<  item.second.get_value<std::string>() << std::endl;
+    std::vector< std::pair<std::string, std::string> > type_defs;
+    auto level_path = _levels[level];
+    pt::read_json("../team-triangle-eduapp/levels" + level_path, root);
 
+    // Get tile defs in order to build tiles when getting values
+    for (pt::ptree::value_type& def : root.get_child("tiledefs")) {
+        std::string img = def.second.get<std::string>("img");
+        std::string type = def.second.get<std::string>("type");
+        type_defs.push_back(std::make_pair(img, type));
     }
 
+    // Get tile values
+    auto x = 0;
+    for (pt::ptree::value_type& row : root.get_child("tiles")) {
+        auto y = 0;
+        for (pt::ptree::value_type& tile : row.second) {
+            std::cout << "coor: " << x << " " << y << " -- val: "
+                      << tile.second.get_value<int>() << " -- typedef: "
+                      << type_defs[tile.second.get_value<int>()].second << " "
+                      << type_defs[tile.second.get_value<int>()].first
+                      << std::endl;
+            y++;
+        }
+        x++;
+    }
+
+    // Place objects
+
+    return texture;
+
+
+
+//    long bounds = (long)root.get_child("tiles").size();
+//    std::cout << bounds << std::endl;
+//    boost::multi_array<int, 2> boost_array_level(
+//            boost::extents[bounds][bounds]);
+//    for (auto& item : root.get_child("tiles")) {
+//        std::cout << item.second.get_value<std::string>() << std::endl;
+//    }
 }
--- tile_set_generator.h
+++ tile_set_generator.h
@@ -1,15 +1,17 @@
 #ifndef TILE_SET_GENERATOR_H
 #define TILE_SET_GENERATOR_H
-#include <boost/multi_array.hpp>
+
 #include <SFML/Graphics.hpp>
-class tile_set_generator
-{
+#include <boost/multi_array.hpp>
+
+class tile_set_generator {
 public:
     tile_set_generator();
     sf::Texture tile_set_generate_level(int Level);
+
 private:
-    //boost::multi_array<int, 1> levels;
-    std::vector<std::string> levels;
+    // boost::multi_array<int, 1> _levels;
+    std::vector<std::string> _levels;
 };
 
 #endif // TILE_SET_GENERATOR_H
